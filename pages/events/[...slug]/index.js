5493327d2d26c07bd79506e0b46874feff7ee237
Ron Almog <ron8132@gmail.com>
2021-10-03 02:37:45 -0400

    add basic pages, images, dummy data

---
+++ pages/events/[...slug]/index.js
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h2>Filtered events</h2>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/index.js
@@ -0,0 +1,9 @@
+const events = () => {
+  return (
+    <div>
+      <h1>Events page</h1>
+    </div>
+  );
+};
+
+export default events;
