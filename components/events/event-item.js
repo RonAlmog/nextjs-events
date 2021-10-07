c208d2a053ca49889c24ee541090f12d3c4a751c
Ron Almog <ron8132@gmail.com>
2021-10-07 02:52:25 -0400

    event detail page

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,9 @@
-import Link from "next/link";
+import Button from "../../ui/button";
 import styles from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrorRightIcon from "../icons/arrow-right-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
   const niceDate = new Date(date).toLocaleDateString("en-US");
@@ -13,14 +17,21 @@ const EventItem = (props) => {
         <div className={styles.summary}>
           <h2>Title</h2>
           <div className={styles.date}>
+            <DateIcon />
             <time>{niceDate}</time>
           </div>
           <div className={styles.address}>
+            <AddressIcon />
             <address>{niceAddress}</address>
           </div>
         </div>
         <div className={styles.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
--- pages/events/[id]/index.js
+++
@@ -1,9 +0,0 @@
-const index = () => {
-  return (
-    <div>
-      <h2>Event page</h2>
-    </div>
-  );
-};
-
-export default index;
