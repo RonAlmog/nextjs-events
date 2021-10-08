95bfb5eb77036534df1a2715bd19267af2318313
Ron Almog <ron8132@gmail.com>
2021-10-08 16:30:22 -0400

    events search, part 1

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,22 @@
-const events = () => {
+import { getAllEvents } from "../../data/dummy-data";
+import EventList from "../../components/events/event-list.js";
+import EventsSearch from "./events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+const AllEvents = () => {
+  const router = useRouter();
+  const allEvents = getAllEvents();
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  };
   return (
-    <div>
-      <h1>Events page</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={allEvents} />
+    </Fragment>
   );
 };
 
-export default events;
+export default AllEvents;
