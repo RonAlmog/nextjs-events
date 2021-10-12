ac8cd7c7a2f45660acc119cd9857f995d8db2e4b
Ron Almog <ron8132@gmail.com>
2021-10-12 16:07:24 -0400

    loading data from firebase

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,22 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { getFeaturedEvents } from "../data/dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
   return (
     <div className={styles.container}>
       <h1>Home page</h1>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+}
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
