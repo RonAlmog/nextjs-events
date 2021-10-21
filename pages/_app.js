df141096c7a8023d36fafe06da5bff59a0250f8a
Ron Almog <ron8132@gmail.com>
2021-10-21 02:58:09 -0400

    head in layout page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Events App</title>
+        <meta name="description" content="my general descriptoin" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
